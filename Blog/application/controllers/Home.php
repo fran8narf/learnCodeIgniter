6a456664f49b2915bfc9e3627c985023c6a32580
fran8narf <srlk_doblev@hotmail.com>
2016-08-13 00:03:43 +0200

    Added homepage to its controller

--- Blog/application/controllers/Home.php
+++ Blog/application/controllers/Home.php
@@ -3,7 +3,11 @@
 class Home extends CI_controller {
 	public function index()
 	{
-		print("Hola mundo en codeigniter!");
+		//Cargamos la vista haciendo uso de $this
+		/*Cargamos el objeto 'load' y después el método
+		  view, al cual le pasamos la dirección relativa
+		  (desde views), de la vista que queramos cargar. */
+		$this->load->view("home");
 	}
 }
 
