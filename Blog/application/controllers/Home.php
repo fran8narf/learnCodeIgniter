b7fd7ae18a353c843b9d01d21f880d8b94c5353e
fran8narf <srlk_doblev@hotmail.com>
2016-08-13 23:07:28 +0200

    Added 'Blog' Styles

--- Blog/application/controllers/Home.php
+++ Blog/application/controllers/Home.php
@@ -6,7 +6,17 @@ class Home extends CI_controller {
 		/*Cargamos el objeto 'load' y después el método
 		  view, al cual le pasamos la dirección relativa
 		  (desde views), de la vista que queramos cargar. */
-		$data = array('title' => 'Home', 'mensaje' => 'Bienvenidos al blog!');
+		$data = array('title' => 'Home');
+		$this->load->view("guest/head", $data);
+
+		$data = array('app' => 'Blog');
+		$this->load->view("guest/nav", $data);
+
+		$data = array('post' => 'Blog', 'descripcion' => 'Bienvenido a mi sitio personal!');
+		$this->load->view("guest/header", $data);
+
+		$this->load->view("guest/content", $data);
+		$this->load->view("guest/footer", $data);
 		$this->load->view("home",$data);
 
 
--- Blog/application/views/home.php
+++ Blog/application/views/home.php
@@ -2,11 +2,15 @@
 <html>
 <head>
 	<title><?= $title ?></title>
-	<link rel="stylesheet" type="text/css" href="<?= base_url() ?>plantilla/css/styles.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css">
 </head>
 <body>
-	<div align="center">
-		<h1><?= $mensaje ?></h1>
+	<div class="col-lg-12 col-md-12">
+		<h1></h1>
+		<button class="btn btn-primary">Aceptar</button>
 	</div>
+<script src="https://code.jquery.com/jquery-3.1.0.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </body>
 </html>
